62ca64bc80e69bc39bc547ed56e254f285e27481
Tequilian <cherryian@outlook.de>
2020-06-11 09:15:51 +0200

    New Shortcut images
    Get from Wikipedia/Wikimedia

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ require "phpClasses/shortcut.php";
 
 //list of links for shortcuts
 $shortcuts = [];
-$shortcuts[0] = ['Netflix', 'https://www.netflix.com/browse', 'https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.ico',''];
+$shortcuts[0] = ['Netflix', 'https://www.netflix.com/browse', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Logo_Netflix.png/800px-Logo_Netflix.png',''];
 $shortcuts[1] = ['Prime', 'https://www.amazon.de/Prime-Video/b?ie=UTF8&node=3279204031', 'https://pbs.twimg.com/profile_images/1176422051432861696/_FpLxtU-_400x400.png',''];
 $shortcuts[2] = ['Disney+', 'https://www.disneyplus.com/de-de/', 'https://prod-static.disney-plus.net/eu-west-1/builds/12f05453babc380d51fabe3a018125ffb8cdbb66_1587566872913/images/logo.svg',''];
-$shortcuts[3] = ['Youtube', 'https://www.youtube.com/', 'https://s.ytimg.com/yts/img/favicon_144-vfliLAfaB.png',''];
+$shortcuts[3] = ['Youtube', 'https://www.youtube.com/', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/71px-YouTube_full-color_icon_%282017%29.svg.png',''];
 $shortcuts[4] = ['ToDo', 'https://to-do.microsoft.com/tasks/myday', 'https://to-do-cdn.microsoft.com/webapp/41d71b1f269fe3d9faed2c1b171f85c4b109d01bac89805a9903f99c50af1150/touch-icon-180x180.png',''];
 
 //list of feeds
