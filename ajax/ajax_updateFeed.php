746e9792912f0c7f38c57fdbd3ffa740188c53b3
Tequilian <cherryian@outlook.de>
2020-10-16 10:50:04 +0200

    Hosting fix

--- ajax/ajax_updateFeed.php
+++ ajax/ajax_updateFeed.php
@@ -3,7 +3,4 @@ require_once '/phpClasses/feedDashboard.php';
 require_once '/sensitiveData.php';
 require_once '/libraries/rss-php/feed.php';
 
-try {
-    echo (new feedDashboard())->updateFeed($_REQUEST["feedType"]);
-} catch (FeedException $e) {
-}
+echo (new feedDashboard())->updateFeed($_REQUEST["feedType"]);
