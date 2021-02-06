faec55dcb7da098872fc0b299e8724b94d90f34c
Kilian Domaratius <github@tequilian.de>
2021-02-06 12:03:53 +0100

    Add https

--- phpClasses/newsFeed.php
+++ phpClasses/newsFeed.php
@@ -22,7 +22,7 @@ class newsFeed extends feedDashboard
         $clients = [];
         $clients['tagesschau'] = array('Tagesschau', 'https://www.tagesschau.de/xml/rss2', 'rss');
         $clients['heise'] = array('Heise', 'https://www.heise.de/rss/heise.rdf', 'rss');
-        $clients['heise-top'] = array('Heise Top News', 'http://www.heise.de/rss/heise-top-atom.xml', 'atom');
+        $clients['heise-top'] = array('Heise Top News', 'https://www.heise.de/rss/heise-top-atom.xml', 'atom');
         $clients['zdnet'] = array('ZDNet', 'https://www.zdnet.de/feed/', 'rss');
 
         //start DIV container for news
